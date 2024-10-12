cfd73d2a78638313ed1bf40322b757900c976c51
yeojijia <yeojijia888@gmail.com>
2024-10-12 13:20:22 +0800

    根据条件隐藏没有添加提示块，在目录下方添加了添加自选按钮

--- src/views/Market/components/Optional.vue
+++ src/views/Market/components/Optional.vue
@@ -1,15 +1,20 @@
 <!-- 自选 -->
 <template>
-    <div class="no_self_selection_block">
-        <div class="no_data_icon">
-            <img src="/static/img/common/no_data.png" alt="暂无数据">
+    <div v-if="watchList.length" :loading="loading">
+        <StockTable style="margin-top:0.1rem" v-if="watchList.length" :loading="loading" @remove="remove"
+            :deleteItem="!!(token)" class="market_optional" :list="watchList" />
+        <div class="addBtn_container">
+            <Button round icon="plus" plain type="primary" hairline="">添加自选</Button>
         </div>
-        <p class="text">你还没有添加自选哦</p>
-        <Button round icon="plus" plain type="primary" hairline="" class="addBtn">添加自选</Button>
     </div>
-    <StockTable style="margin-top:0.1rem" v-if="watchList.length" :loading="loading" @remove="remove"
-        :deleteItem="!!(token)" class="market_optional" :list="watchList" />
     <div v-else-if="!watchList.length && !loading" style="position: relative">
+        <div class="no_self_selection_block">
+            <div class="no_data_icon">
+                <img src="/static/img/common/no_data.png" alt="暂无数据">
+            </div>
+            <p class="text">你还没有添加自选哦</p>
+            <Button round icon="plus" plain type="primary" hairline="" class="addBtn">添加自选</Button>
+        </div>
         <Teleport to=".page_market">
             <div class="one_click_to_favorite_container">
                 <Button round block type="primary" size="large" class="one_click_to_favorite" :loading="addLoading"
@@ -311,6 +316,12 @@ const remove = item => {
         background-color: #014CFA;
         color: #FFFFFF;
     }
+}
+
+.addBtn_container {
+    margin-top: 0.398rem;
+    display: flex;
+    justify-content: center;
 }
 
 .search_block {
