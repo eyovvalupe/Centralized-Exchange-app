1899076c313424968a682630753f058f722c8d2c
willam <willam@gmail.com>
2024-10-23 18:04:18 +0800

    增加语言配置

---
+++ src/i18/lang-zh.js
@@ -0,0 +1,26 @@
+
+export default {
+    
+    assets:{
+        recharge:"充值",
+        withdraw:"提现",
+        transfer:"划转",
+        financialRecords:"资金记录"
+    },
+    topUpCrypto:{
+        recharge:"充值",
+        rechargeRecord:"充值记录",
+        cryptocurrency:"加密货币",
+        bankCard:"银行卡",
+        currency:"币种",
+        network:"网络",
+        rechargeAmount:"充值金额",
+        inputPlaceholder:"请输入",
+        confirm:"确定",
+        currencySelection:"币种选择",
+        searchPlaceholder:"搜索",
+        enterCurrency:"输入币种",
+        networkSelection:"网络选择"
+
+    }
+}
