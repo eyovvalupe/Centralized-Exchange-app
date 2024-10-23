781d2d93dfb85c19cabcc2c48fe6bd48a2d229b4
willam <willam@gmail.com>
2024-10-23 18:04:12 +0800

    资产-加密货币充值、币种选择、充值确认页 UI调整

--- src/views/assets/Recharging.vue
+++ src/views/assets/Recharging.vue
@@ -9,32 +9,27 @@
             </template>
         </Top>
 
-        <div class="form">
-            <div class="amount">
+        <div class="recharge_box">
 
-                <div class="num" @click="copyPrice">
-                    <span style="border-bottom:1px dashed #D0D2D6">{{ form.amount }}<b
-                            style="font-size: 0.48rem;color:#333">{{
-                                form.name
-                            }}</b></span>
-                    <div class="copy_icon" v-if="form.amount">
-                        <img src="/static/img/common/copy2.png" alt="img">
-                    </div>
+            <div class="time_box" v-if="s && !loading">
+                <div class="time_box_label">充值倒计时</div>
+                <div class="time">{{ showS }}</div>
+                <div class="network-tag" v-if="form.name">
+                    <span>{{form.name}} · {{form.network }}</span>
                 </div>
-                <div v-if="rate" style="margin: 0 auto 0.32rem auto;width: 3.34rem;text-align: right;">{{
-                    ratePrice }}
-                    USDT</div>
-                <div v-else style="height: 0.8rem;"></div>
+                <div class="amount" @click="copyPrice">
+                    {{ form.amount }}
+                    <span>{{form.name}}</span>
+                    <!-- <div v-if="rate">{{ratePrice }} USDT</div> -->
 
-                <div v-if="form.name"
-                    style="background-color: #F6F6F6;color: #3830DD;display: inline-block;padding: 0 0.2rem;margin-bottom:0.04rem">
-                    {{
-                        form.name
-                    }} · {{
-                        form.network }}</div>
+                </div>
+                <div class="network-tag">
+                    <span style="color:#000;">$20,991.18</span>
+                </div>
             </div>
+        </div>
 
-            <div v-if="!form.amount" style="height: 1rem;"></div>
+        <div class="recharge_box">
             <div class="qrcode_box">
                 <Loading :loading="loading" v-show="loading" />
                 <div id="qrcode" ref="qrcodeRef" v-show="!loading"></div>
@@ -50,15 +45,20 @@
                     <div>二维码已过期</div>
                 </div>
             </div>
-            <div class="address" @click="copy" v-if="address">
-                <span>{{ address }}</span>
-                <div class=" copy_icon">
-                    <img src="/static/img/common/copy2.png" alt="img">
-                </div>
+
+            <div class="address" v-if="address">
+                <div class="address_label">收款人钱包地址</div>
+                <div class="address_val">{{ address }}</div>
+                <div class="address_copy_btn" @click="copy">复制</div>
+            </div>
+
+            <div class="tip">
+                提示：请在截至时间内完成充值<br/>
+                订单到期作废
             </div>
         </div>
 
-        <div class="circle_box">
+        <!-- <div class="circle_box">
             <Circle class="circle" :start-position="'right'" :stroke-linecap="'butt'" :stroke-width="150"
                 :layer-color="'#E5E5E5'" :color="'#014CFA'" size="40px" v-if="s && !loading"
                 v-model:current-rate="currentRate" :rate="0" :text="''" />
@@ -66,17 +66,10 @@
                 <div class="time">{{ showS }}</div>
                 <div>截至时间</div>
             </div>
-        </div>
-        <div class="tip">
-            <div>提示：请在<span style="color:#FD4938">截至时间</span>内完成充值</div>
-            <div>订单到期作废</div>
-        </div>
+        </div> -->
+        
         <div class="btns">
-            <!-- <Button round color="#EFF6FF" class="submit" type="info" @click="router.back()">
-                <span style="color:#014CFA">取消</span>
-            </Button>
-            <Button @click="openSure" :loading="loading" round color="#014CFA" class="submit" type="primary">确定</Button> -->
-
+           
             <Button v-if="orderStatus == 'success'" @click="router.back()" :loading="loading" round color="#18B762"
                 style="width:100%" class="submit" type="info"><span style="color:#fff">成功</span></Button>
             <Button v-else-if="orderStatus == 'failure'" @click="router.back()" :loading="loading" round color="#E8503A"
@@ -278,7 +271,7 @@ const copyPrice = () => {
 
 <style lang="less" scoped>
 .page_recharge {
-    padding: 1.4rem 0.32rem 0.2rem 0.32rem;
+    padding: 1.12rem 0.32rem 0.32rem 0.32rem;
 
     position: relative;
 
@@ -287,29 +280,53 @@ const copyPrice = () => {
         height: 0.4rem;
     }
 
-    .form {
+    .recharge_box {
+        border-radius: 0.32rem;
+        margin-top: 0.2rem;
+        background: #FFF;
+        box-shadow: 0px 0.02rem 0.2rem 0 rgba(1, 76, 250, 0.20);
+        padding:0.32rem;
         .amount {
+            color: #014CFA;
             text-align: center;
-            color: #000000;
-            font-size: 0.28rem;
-            line-height: 0.48rem;
+            font-size: 0.48rem;
+            font-weight: 600;
+            line-height: 0.36rem;
+            span{
+                font-size: 0.32rem;
+                font-weight: 400;
+            }
+        }
+
+        .time_box {
+            margin-left: 0.12rem;
+            color: #666;
+            font-size: 0.24rem;
             font-weight: 400;
-
-            .num {
-                font-weight: 600;
-                font-size: 0.8rem;
-                line-height: 0.9rem;
-                word-break: break-all;
+            text-align: left;
+            &_label{
+                color: #666D80;
+                text-align: center;
+                font-size: 0.28rem;
+                line-height: 0.36rem; 
+            }
+            .time {
+                color: #E8503A;
+                font-size: 0.32rem;
+                font-weight: 500;
+                margin-bottom: 0.07rem;
+            }
+            .network-tag{
                 display: flex;
-                align-items: center;
                 justify-content: center;
-
-                .copy_icon {
-                    width: 0.4rem;
-                    height: 0.4rem;
-                    line-height: 0;
-                    position: relative;
-                    left: 0.2rem;
+                align-content: center;
+                span{
+                    height: 0.6rem;
+                    padding: 0 0.28rem;
+                    border-radius: 0.5rem;
+                    background-color:#F5F7FC;
+                    color: #014CFA;
+                    font-size: 0.28rem;
                 }
             }
         }
@@ -391,56 +408,25 @@ const copyPrice = () => {
         }
     }
 
-    .circle_box {
-        margin: 0.32rem auto 0.24rem auto;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-
-        .circle {
-            display: block;
-        }
-
-        .time_box {
-            margin-left: 0.12rem;
-            color: #666;
-            font-size: 0.24rem;
-            font-weight: 400;
-            text-align: left;
-
-            .time {
-                color: #E8503A;
-                font-size: 0.32rem;
-                font-weight: 500;
-                margin-bottom: 0.07rem;
-            }
-        }
-    }
-
 
     .tip {
-        font-weight: 400;
-        color: #191B1E;
-        font-size: 0.24rem;
-        line-height: 0.32rem;
-        margin: 0 auto 0.4rem auto;
+        color: #8F92A1;
         text-align: center;
-        background-color: #F2F1F9;
-        width: 75vw;
-        color: #333;
-        padding: 0.12rem 0.5rem;
-        line-height: 0.4rem;
-        border-radius: 0.04rem;
+        font-family: Inter;
+        font-size: 0.24rem;
+        font-style: normal;
+        font-weight: 400;
+        line-height: 0.32rem; 
     }
 
     .btns {
         display: flex;
         align-items: center;
         justify-content: space-between;
-        margin: 0.68rem 0 0.4rem 0;
-
+        margin-top: 0.6rem;
+        padding: 0 0.32rem;
         .submit {
-            width: 47%;
+            width: 100%;
             height: 1.12rem;
         }
     }
