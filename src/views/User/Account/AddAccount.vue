02d6bfd02792a018c15ab685f7ea0c1a7d37314b
eyovvalupe <rylantwcn705172@hotmail.com>
2024-10-29 10:23:21 +0800

    feat:修改币种选项UI

--- src/views/User/Account/AddAccount.vue
+++ src/views/User/Account/AddAccount.vue
@@ -14,115 +14,184 @@
     >
       <Tab :title="'加密货币'" name="0">
         <div class="tab_data">
-          <div class="no_data" v-if="cryptoList.length == 0">
-            <img src="/static/img/user/noData.svg" />
-            <span class="mt-[0.2rem] text-[#a4acb9] text-[0.28rem]"
-              >暂无数据</span
-            >
-          </div>
-          <div class="list" v-else>
-            <Crypto />
+          <div class="list">
+            <div class="page_crypto">
+              <div class="flex flex-col items-center">
+                <div style="float: left">
+                  <div class="subtitle">币种</div>
+                  <div class="item" @click="showCrypto = true">
+                    <div class="item_icon">
+                      <img
+                        :src="`/static/img/crypto/${form.currency}.png`"
+                        alt="usdt"
+                      />
+                    </div>
+                    <div class="ipt">{{ form.currency }}</div>
+                    <Icon style="transform: rotate(90deg)" name="play" />
+                  </div>
+                </div>
+                <div style="float: left">
+                  <div class="subtitle">网络</div>
+                  <div class="item" @click="showNet = true">
+                    <div class="ipt">{{ form.network }}</div>
+                    <Icon style="transform: rotate(90deg)" name="play" />
+                  </div>
+                </div>
+                <div style="float: left">
+                  <div class="subtitle">地址</div>
+                  <div class="item">
+                    <input
+                      type="text"
+                      v-model.trim="form.address"
+                      class="ipt"
+                      maxlength="50"
+                      placeholder="请输入对应币种地址"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div class="btn_container">
+                <Button
+                  :disabled="!form.address"
+                  class="submit"
+                  type="primary"
+                  round
+                  color="#014CFA"
+                  @click="next"
+                  >保存</Button
+                >
+              </div>
+              <!-- 谷歌验证 -->
+              <GoogleVerfCode ref="googleRef" @submit="submit" />
+            </div>
           </div>
         </div>
       </Tab>
       <Tab :title="'银行卡'" name="1">
         <div class="tab_data">
-          <div class="no_data" v-if="bankList.length == 0">
-            <img src="/static/img/user/noData.svg" />
-            <span class="mt-[0.2rem] text-[#a4acb9] text-[0.28rem]"
-              >暂无数据</span
-            >
-          </div>
-          <div class="list" v-else>
+          <div class="list">
             <Bank />
           </div>
         </div>
       </Tab>
     </Tabs>
+    <!-- 币种 -->
+    <Popup
+      :safe-area-inset-top="false"
+      :safe-area-inset-bottom="true"
+      round
+      v-model:show="showCrypto"
+      position="bottom"
+      class="popup_container"
+    >
+      <div class="bottoms">
+        <div
+          @click="chooseCurrency(item)"
+          class="bottom"
+          :class="{ active_bottom: form.currency == item }"
+          v-for="item in currencyMapList"
+          :key="item"
+        >
+          <div class="bottom_icon_container">
+            <img
+              :src="`/static/img/crypto/${item.toUpperCase()}.png`"
+              alt="usdt"
+            />
+          </div>
+          <span>{{ item }}</span>
+        </div>
+        <Icon @click="showCrypto = false" class="close" name="cross" />
+      </div>
+    </Popup>
+    <!-- 网络 -->
+    <Popup
+      :safe-area-inset-top="true"
+      :safe-area-inset-bottom="true"
+      round
+      v-model:show="showNet"
+      position="bottom"
+      class="popup_container"
+    >
+      <div class="bottoms">
+        <div
+          @click="chooseNet(item)"
+          class="bottom"
+          :class="{ active_bottom: form.network == item }"
+          v-for="item in currNetwork"
+          :key="item"
+        >
+          <span>{{ item.network }}</span>
+        </div>
+        <Icon @click="showNet = false" class="close" name="cross" />
+      </div>
+    </Popup>
   </div>
 </template>
 
 <script setup>
-import { Tabs, Tab, Icon, ActionSheet, showConfirmDialog } from "vant";
+import {
+  Tabs,
+  Tab,
+  Icon,
+  showConfirmDialog,
+  Button,
+  Popup,
+  showToast,
+} from "vant";
 import store from "@/store";
 import { computed, ref, onMounted } from "vue";
 import router from "@/router";
 import { _hiddenAccount } from "@/utils/index";
 import { useRoute, useRouter } from "vue-router";
-import { _userinfo } from "@/api/api";
+import { _userinfo, _addAccount, _cryptoCoin } from "@/api/api";
 import Bank from "./Bank.vue";
-import Crypto from "./Crypto.vue";
-import Check from "./Check.vue";
+import GoogleVerfCode from "@/components/GoogleVerfCode.vue";
 import Top from "@/components/Top.vue";
 
 const route = useRoute();
+const coinMap = computed(() => store.state.coinMap || {});
+const googleRef = ref();
+const loading = ref(false);
+const form = ref({
+  channel: "crypto",
+  currency: route.query.currency || "USDT",
+  network: "",
+  address: "",
+});
 
-store.dispatch("updateAccountList");
+// 币种
+const showCrypto = ref(false);
+const chooseCurrency = (item) => {
+  form.value.currency = item;
+  showCrypto.value = false;
+  initNetwork();
+};
+
+// 网络
+const showNet = ref(false);
+const chooseNet = (item) => {
+  form.value.network = item.network;
+  showNet.value = false;
+};
+
+const currNetwork = computed(() => {
+  return coinMap.value[form.value.currency] || [];
+});
+
+setTimeout(() => {
+  console.error("????", coinMap.value, form.value.currency, currNetwork.value);
+}, 5000);
+const initNetwork = () => {
+  if (currNetwork.value[0]) {
+    form.value.network = currNetwork.value[0].network;
+  }
+};
+initNetwork();
+
 const selectedPayment = computed(() => store.state.selectedPayment || "0");
-const userInfo = computed(() => store.state.userInfo || {});
-const accountList = computed(() => store.state.accountList || []); // 收款方式列表
-const bankList = computed(() =>
-  accountList.value.filter((item) => item.channel == "bank")
-); // 银行卡
-const cryptoList = computed(() =>
-  accountList.value.filter((item) => item.channel == "crypto")
-); // 加密货币
-// tabs
 const active = ref(selectedPayment.value);
 const changeTab = (key) => {
   active.value = key;
-};
-
-// 打开添加类型选择弹窗
-const showAS = ref(false);
-const actions = [
-  { name: "银行卡", value: "bank" },
-  { name: "加密货币", value: "crypto" },
-];
-const goAddAccount = async () => {
-  console.log(userInfo.value);
-  //身份认证检测
-  if (userInfo.value.kycl2 != 2) {
-    return showConfirmDialog({
-      title: "身份未认证",
-      message: "您的身份还未进行认证，请先认证再添加收款账户",
-      confirmButtonText: "去认证",
-    }).then(() => jump("kyc"));
-  }
-
-  // google检测
-  if (userInfo.value.kycl2 == 2 && !userInfo.value.googlebind) {
-    return showConfirmDialog({
-      title: "谷歌验证器未绑定",
-      message: "您的谷歌验证器还未绑定，请先绑定再添加收款账户",
-    }).then(() => {
-      jump("google");
-    });
-  }
-  // showAS.value = true;
-  jump("crypto");
-};
-const onSelect = async (item) => {
-  showAS.value = false;
-  if (item.value == "bank") {
-    // 银行卡
-    if (userInfo.value.kycl2 != 2) {
-      return showConfirmDialog({
-        title: "身份认证",
-        message: "你还未完成身份认证，是否去认证?",
-      }).then(() => {
-        jump("kyc");
-      });
-    }
-  }
-  jump(item.value);
-};
-
-// 添加
-const jump = (name) => {
-  router.push({
-    name,
-  });
 };
 
 const pageLoading = ref(true);
@@ -131,11 +200,6 @@ onMounted(() => {
     pageLoading.value = false;
   }, 500);
 });
-
-Promise.all([
-  import("@/views/User/Account/Bank.vue"),
-  import("@/views/User/Account/Crypto.vue"),
-]);
 
 const goBack = () => {
   if (route.query.reurl) {
@@ -149,16 +213,109 @@ const goBack = () => {
     router.back();
   }
 };
+
+const currencyMapList = computed(() => {
+  return Object.keys(coinMap.value) || [];
+});
+_cryptoCoin({ dedup: false }).then((res) => {
+  store.commit("setCoinMap", res.data || []);
+});
+
+// 提交
+const submit = (googleCode) => {
+  if (loading.value) return;
+  loading.value = true;
+  const params = {
+    ...form.value,
+    googlecode: googleCode,
+    token: sessionToken.value,
+  };
+  _addAccount(params)
+    .then((res) => {
+      if (res.code == 200) {
+        showToast("添加成功");
+        setTimeout(() => {
+          router.back();
+        }, 200);
+      }
+    })
+    .finally(() => {
+      getSessionToken();
+      loading.value = false;
+    });
+};
+const next = () => {
+  googleRef.value.open();
+};
+
+// sessionToken
+const sessionToken = computed(() => store.state.sessionToken || "");
+const getSessionToken = () => {
+  loading.value = true;
+  store.dispatch("updateSessionToken").finally(() => {
+    loading.value = false;
+  });
+};
+getSessionToken();
 </script>
 
 <style lang="less" scoped>
 .page_account {
+  width: 7.5rem;
   display: flex;
   flex-direction: column;
-  padding: 0 0.32rem;
   height: 100%;
+  .popup_container {
+    width: 7.5rem;
+
+    .bottoms {
+      width: 100%;
+      height: 50vh;
+      position: relative;
+      padding: 1.12rem 0;
+
+      :deep(.van-popup--bottom.van-popup--round) {
+        width: 6.86rem;
+      }
+      .close {
+        position: absolute;
+        font-size: 0.4rem;
+        top: 0.3rem;
+        right: 0.4rem;
+        color: #161616;
+      }
+
+      .bottom {
+        color: #111111;
+        font-size: 0.28rem;
+        height: 1.12rem;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        overflow: hidden;
+        border-bottom: 0.02rem solid #f5f5f5;
+
+        &:last-child {
+          border-bottom: none;
+        }
+
+        .bottom_icon_container {
+          width: 0.44rem;
+          height: 0.44rem;
+          margin-right: 0.14rem;
+          position: relative;
+          top: -0.04rem;
+        }
+      }
+
+      .active_bottom {
+        background-color: #f5f5f5;
+      }
+    }
+  }
 
   :deep(.van-tabs__wrap) {
+    padding: 0 0.32rem;
     height: 0.92rem;
     margin-bottom: 0.4rem;
     margin-top: 1.2rem;
@@ -226,6 +383,74 @@ const goBack = () => {
     border-radius: 1.6rem;
     position: absolute;
     bottom: 0.72rem;
+  }
+
+  .page_crypto {
+    height: calc(100vh - 2.52rem);
+
+    .subtitle {
+      font-weight: 400;
+      font-size: 0.28rem;
+      line-height: 0.36rem;
+      color: #061023;
+      margin-bottom: 0.15rem;
+    }
+
+    .item {
+      width: 6.86rem;
+      // padding: 0 0.32rem;
+      height: 0.92rem;
+      border: 0.02rem solid #d0d8e2;
+      border-radius: 0.32rem;
+      margin-bottom: 0.4rem;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      padding: 0 0.36rem;
+
+      .ipt {
+        flex: 1;
+        height: 100%;
+        font-size: 0.3rem;
+        text-align: left;
+        outline: none;
+        border: none;
+        background-color: rgba(0, 0, 0, 0);
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+        font-weight: 600;
+        color: #061023;
+      }
+
+      .item_icon {
+        width: 0.48rem;
+        height: 0.48rem;
+        margin-right: 0.2rem;
+      }
+
+      :deep(.van-icon) {
+        color: #666d80;
+      }
+    }
+
+    .btn_container {
+      width: 100%;
+      display: flex;
+      justify-content: center;
+      position: relative;
+
+      .submit {
+        margin-top: 0.4rem;
+        height: 1rem;
+        width: 6.86rem;
+
+        :deep(.van-button__text) {
+          font-size: 0.36rem;
+          line-height: 0.64rem;
+        }
+      }
+    }
   }
 }
 </style>
--- src/views/User/Account/Bank.vue
+++ src/views/User/Account/Bank.vue
@@ -1,142 +1,167 @@
 <!-- 添加银行卡 -->
 <template>
-    <div class="page page_bank">
-        <div class="subtitle">持卡人姓名</div>
-        <div class="item">
-            <span style="flex: 1;">{{ name || '--' }}</span>
-            <div class="icon_ok">
-                <img src="/static/img/user/ok.png" alt="ok">
-            </div>
-            <span>已认证</span>
+  <div class="page page_bank">
+    <div style="flex: 1; margin-bottom: 0.8rem">
+      <div class="subtitle">持卡人姓名</div>
+      <div class="item">
+        <span style="flex: 1">{{ name || "--" }}</span>
+        <div class="icon_ok">
         </div>
-        <div class="subtitle">银行</div>
-        <div class="item">
-            <input v-model="form.bank_name" type="text" class="ipt" maxlength="50">
-        </div>
-        <div class="subtitle">卡号</div>
-        <div class="item">
-            <input v-model.trim="form.bank_card_number" type="text" class="ipt" maxlength="50">
-        </div>
-
-        <div style="flex: 1;"></div>
-        <Button class="submit" :disabled="!(form.bank_name && form.bank_card_number)" :loading="loading" type="primary"
-            round color="#014CFA" @click="next">保存</Button>
-
-        <!-- 谷歌验证 -->
-        <GoogleVerfCode ref="googleRef" @submit="submit" />
+        <span>已认证</span>
+      </div>
+      <div class="subtitle">银行</div>
+      <div class="item">
+        <input
+          v-model="form.bank_name"
+          type="text"
+          class="ipt"
+          maxlength="50"
+          placeholder="请输入银行名称"
+        />
+      </div>
+      <div class="subtitle">卡号</div>
+      <div class="item">
+        <input
+          v-model.trim="form.bank_card_number"
+          type="text"
+          class="ipt"
+          maxlength="50"
+          placeholder="请输入银行卡卡号"
+        />
+      </div>
     </div>
+    <Button
+      class="submit"
+      :disabled="!(form.bank_name && form.bank_card_number)"
+      type="primary"
+      round
+      color="#014CFA"
+      @click="next"
+      >保存</Button
+    >
+
+    <!-- 谷歌验证 -->
+    <GoogleVerfCode ref="googleRef" @submit="submit" />
+  </div>
 </template>
 
 <script setup>
-import GoogleVerfCode from "@/components/GoogleVerfCode.vue"
-import Top from '@/components/Top.vue';
-import { Button, showToast } from "vant"
-import { ref, computed } from "vue"
-import { _kycGet, _addAccount } from "@/api/api"
+import GoogleVerfCode from "@/components/GoogleVerfCode.vue";
+import Top from "@/components/Top.vue";
+import { Button, showToast } from "vant";
+import { ref, computed } from "vue";
+import { _kycGet, _addAccount } from "@/api/api";
 import router from "@/router";
 import store from "@/store";
 
-const googleRef = ref()
+const googleRef = ref();
 
-const loading = ref(false)
+const loading = ref(false);
 const form = ref({
-    channel: 'bank',
-    // currency: null,
-    // network: null,
-    // address: null,
-    account_name: '',
-    bank_name: '',
-    bank_card_number: '',
-})
+  channel: "bank",
+  // currency: null,
+  // network: null,
+  // address: null,
+  account_name: "",
+  bank_name: "",
+  bank_card_number: "",
+});
 
 // 提交
 const submit = (googleCode) => {
-    if (loading.value) return
-    loading.value = true
-    _addAccount({
-        ...form.value,
-        googlecode: googleCode,
-        token: sessionToken.value
-    }).then(res => {
-        if (res.code == 200) {
-            showToast('添加成功')
-            setTimeout(() => {
-                router.back()
-            }, 200)
-        }
-    }).finally(() => {
-        loading.value = false
+  if (loading.value) return;
+  loading.value = true;
+  _addAccount({
+    ...form.value,
+    googlecode: googleCode,
+    token: sessionToken.value,
+  })
+    .then((res) => {
+      if (res.code == 200) {
+        showToast("添加成功");
+        setTimeout(() => {
+          router.back();
+        }, 200);
+      }
     })
-}
+    .finally(() => {
+      loading.value = false;
+    });
+};
 const next = () => {
-    googleRef.value.open()
-}
+  googleRef.value.open();
+};
 
 // sessionToken
-const sessionToken = computed(() => store.state.sessionToken || '')
+const sessionToken = computed(() => store.state.sessionToken || "");
 const getSessionToken = () => {
-    loading.value = true
-    store.dispatch('updateSessionToken').finally(() => {
-        loading.value = false
-    })
-}
-getSessionToken()
+  loading.value = true;
+  store.dispatch("updateSessionToken").finally(() => {
+    loading.value = false;
+  });
+};
+getSessionToken();
 
 // 获取真实姓名
-const name = ref('')
-// _kycGet().then(res => {
-//     name.value = res?.data?.name || ''
-//     form.value.account_name = res?.data?.name || ''
-// })
+const name = ref("");
+_kycGet().then(res => {
+    name.value = res?.data?.name || ''
+    form.value.account_name = res?.data?.name || ''
+})
 </script>
 
 <style lang="less" scoped>
 .page_bank {
-    height: 100%;
+  height: 100%;
+  padding: 0 0.32rem;
+
+  .subtitle {
+    font-weight: 400;
+    font-size: 0.28rem;
+    line-height: 0.36rem;
+    color: #061023;
+    margin-bottom: 0.15rem;
+  }
+
+  .item {
+    width: 100%;
+    height: 0.92rem;
+    border: 0.02rem solid #d0d8e2;
+    border-radius: 0.32rem;
+    margin-bottom: 0.4rem;
     display: flex;
-    flex-direction: column;
+    align-items: center;
     justify-content: space-between;
-    align-items: flex-start;
+    padding: 0 0.44rem;
 
-    .subtitle {
-        font-weight: 400;
-        font-size: 0.28rem;
-        line-height: 0.36rem;
-        color: #333333;
-        margin-bottom: 0.15rem;
+    .ipt {
+      width: 100%;
+      height: 100%;
+      font-size: 0.24rem;
+      text-align: left;
+      outline: none;
+      border: none;
+      background-color: rgba(0, 0, 0, 0);
     }
+  }
 
-    .item {
-        width: 100%;
-        height: 1.12rem;
-        border: 1px solid #D0D8E2;
-        border-radius: 0.32rem;
-        margin-bottom: 0.22rem;
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        padding: 0 0.44rem;
+  .submit {
+    height: 1.12rem;
+    width: 100%;
 
-        .ipt {
-            width: 100%;
-            height: 100%;
-            font-size: 0.24rem;
-            text-align: left;
-            outline: none;
-            border: none;
-            background-color: rgba(0, 0, 0, 0);
-        }
-
-        .icon_ok {
-            width: 0.32rem;
-            height: 0.32rem;
-            margin: 0 0.12rem;
-        }
+    :deep(.van-button__text) {
+      font-size: 0.36rem;
+      line-height: 0.64rem;
     }
+  }
 
-    .submit {
-        height: 1.12rem;
-        width: 100%;
+  :deep(.icon_ok) {
+      width: 0.4rem;
+      height: 0.4rem;
+      margin-right: 0.1rem;
+      background-image: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none"><path d="M17.241 4.9285L10.783 1.1875C10.354 0.9375 9.665 0.9375 9.236 1.1875L8.158 1.8075L2.768 4.9285C2.339 5.1785 2 5.7785 2 6.2685V13.7405C2 14.2305 2.349 14.8405 2.769 15.0805L4.625 16.1505L9.226 18.8115C9.655 19.0615 10.344 19.0615 10.773 18.8115L17.231 15.0705C17.66 14.8205 18 14.2205 18 13.7305V6.2685C18.01 5.7785 17.66 5.1785 17.241 4.9285Z" fill="white"/><path d="M17.241 4.9285L10.783 1.1875C10.354 0.9375 9.665 0.9375 9.236 1.1875L8.158 1.8075L2.768 4.9285C2.339 5.1785 2 5.7785 2 6.2685V13.7405C2 14.2305 2.349 14.8405 2.769 15.0805L4.625 16.1505L9.226 18.8115C9.655 19.0615 10.344 19.0615 10.773 18.8115L17.231 15.0705C17.66 14.8205 18 14.2205 18 13.7305V6.2685C18.01 5.7785 17.66 5.1785 17.241 4.9285Z" fill="green"/><path d="M10.0002 12.7507C9.90172 12.751 9.80415 12.7317 9.71316 12.6939C9.62218 12.6561 9.53961 12.6007 9.47023 12.5307L6.47023 9.53074C6.40056 9.46107 6.3453 9.37837 6.3076 9.28734C6.26989 9.19632 6.25049 9.09876 6.25049 9.00024C6.25049 8.90172 6.26989 8.80416 6.3076 8.71314C6.3453 8.62211 6.40056 8.53941 6.47023 8.46974C6.53989 8.40007 6.6226 8.34481 6.71362 8.30711C6.80465 8.26941 6.90221 8.25 7.00073 8.25C7.09925 8.25 7.19681 8.26941 7.28783 8.30711C7.37886 8.34481 7.46156 8.40007 7.53123 8.46974L10.0002 10.9397L13.4702 7.46974C13.6109 7.32904 13.8018 7.25 14.0007 7.25C14.1997 7.25 14.3905 7.32904 14.5312 7.46974C14.6719 7.61044 14.751 7.80126 14.751 8.00024C14.751 8.19922 14.6719 8.39004 14.5312 8.53074L10.5312 12.5307C10.4616 12.6006 10.3788 12.656 10.2877 12.6938C10.1966 12.7315 10.0989 12.7509 10.0002 12.7507Z" fill="white"/></svg>');
+      background-size: cover;
+      display: inline-block;
     }
 }
 </style>
