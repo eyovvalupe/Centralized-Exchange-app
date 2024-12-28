fce3ff0b6f9955be3758e40213ee178cbfbdd0f6
eyovvalupe <rylantwcn705172@hotmail.com>
2024-12-28 15:15:59 -0800

    feat:修改k图分时图的小bug

--- src/components/KlineCharts/AreaChart.vue
+++ src/components/KlineCharts/AreaChart.vue
@@ -21,23 +21,21 @@ import { init, dispose } from 'klinecharts'
 import { klineConfig } from './kline.conf';
 import { _time } from "@/api/api"
 import { useSocket } from '@/utils/ws'
-import { Loading } from "vant"
 import { _maxTail } from "@/utils/index"
+import { Loading } from "vant"
 
 const { startSocket } = useSocket()
 let socket = null
 
 const props = defineProps({
     symbol: '',
-    color: '',
-    rgbColor: '',
     showY: {
         type: Boolean,
         default: false
     }
 })
 const symbol = computed(() => props.symbol || '')
-watch(() => props.symbol, val => {
+watch(symbol, val => {
     setTimeout(() => {
         initData()
     }, 0)
@@ -52,112 +50,76 @@ onMounted(() => {
         chart?.setOffsetRightDistance(props.showY ? 50 : 0) // 设置右边距
         chart?.setMaxOffsetLeftDistance(0) // 设置左边最大空出的边距
         chart?.setMaxOffsetRightDistance(props.showY ? 50 : 0) // 设置右边最大空出的边距
+        const selfStyle = {
+            grid: {
+                vertical: {
+                    show: false
+                }
+            },
+            xAxis: {
+                show: props.showY,
+                size: 'auto'
+            },
+            yAxis: {
+                show: props.showY
+            },
+            candle: {
+                type: 'area',
+                area: {
+                    point: {
+                        show: props.showY
+                    }
+                },
+                priceMark: {
+                    show: props.showY
+                },
+                tooltip: {
+                    offsetLeft: 2,
+                    offsetTop: 3,
+                    offsetRight: 2,
+                    offsetBottom: 3,
+                    showRule: 'follow_cross', /// ,follow_cross
+                    showType: 'rect',
+                    custom: [
+                        { title: 'time', value: '{time}' },
+                        // { title: 'open', value: '{open}' },
+                        // { title: 'high', value: '{high}' },
+                        // { title: 'low', value: '{low}' },
+                        // { title: 'close', value: '{close}' },
+                        { title: 'price', value: '{open}' }
+                    ],
+                    text: {
+                        size: 10,
+                        family: 'Helvetica Neue',
+                        weight: 'normal',
+                        color: '#121826',
+                        marginLeft: 8,
+                        marginTop: 4,
+                        marginRight: 8,
+                        marginBottom: 4
+                    },
+                }
+            },
+        }
+        if (props.showY) {
+            delete selfStyle.yAxis
+        }
+        chart.setStyles(selfStyle)
+        window.addEventListener('resize', resetSize)
     }
     initData()
 })
 onBeforeUnmount(() => {
     dispose('chat_area')
     socket && socket.emit('time', '') // 取消订阅
+    window.removeEventListener('resize', resetSize)
 })
 
 
 const loading = ref(true)
-let timeout = null
 const initData = async () => {
-    // 样式
-    const selfStyle = {
-        grid: {
-            show: true,
-            horizontal: {
-                show: true,
-                size: 1,
-                color: '#EDEDED',
-                style: 'dashed',
-                dashedValue: [2, 2]
-            },
-            vertical: {
-                show: true,
-                size: 1,
-                color: '#EDEDED',
-                style: 'dashed',
-                dashedValue: [2, 2]
-            }
-        },
-        xAxis: {
-            show: props.showY,
-            size: 'auto'
-        },
-        yAxis: {
-            show: props.showY
-        },
-        candle: {
-            type: 'area',
-            priceMark: {
-                show: props.showY
-            },
-            area: {
-                lineColor: props.color || "#2168F6",
-                point: {
-                    show: props.showY
-                },
-                lineSize: 2,
-                backgroundColor: [
-                    {
-                        offset: 0,
-                        color: props.color ? `rgba(${props.rgbColor}, 0.01)` : "rgba(0, 0, 168, 0.01)"
-                    },
-                    {
-                        offset: 1,
-                        color: props.color ? `rgba(${props.rgbColor}, 0.3)` : "rgba(0, 0, 168, 0.3)"
-                    }
-                ],
-                smooth: true,
-                point: {
-                    show: true,
-                    color: props.color ? `rgba(${props.rgbColor}, 0.3)` : "rgba(0, 0, 168, 0.3)",
-                    radius: 4,
-                    rippleColor: props.color ? `rgba(${props.rgbColor}, 0.3)` : "rgba(0, 0, 168, 0.3)", // getAlphaBlue(0.3),
-                    rippleRadius: 8,
-                    animation: true,
-                    animationDuration: 1000
-                }
-            },
-            tooltip: {
-                offsetLeft: 2,
-                offsetTop: 3,
-                offsetRight: 2,
-                offsetBottom: 3,
-                showRule: 'follow_cross', /// ,follow_cross
-                showType: 'rect',
-                custom: [
-                    { title: 'time', value: '{time}' },
-                    // { title: 'open', value: '{open}' },
-                    // { title: 'high', value: '{high}' },
-                    // { title: 'low', value: '{low}' },
-                    // { title: 'close', value: '{close}' },
-                    { title: 'price', value: '{open}' }
-                ],
-                text: {
-                    size: 10,
-                    family: 'Helvetica Neue',
-                    weight: 'normal',
-                    color: '#121826',
-                    marginLeft: 8,
-                    marginTop: 4,
-                    marginRight: 8,
-                    marginBottom: 4
-                },
-            }
-        },
-    }
-    if (props.showY) {
-        delete selfStyle.yAxis
-    }
-    chart.setStyles(selfStyle)
-    // 数据
     const query = symbol.value
     loading.value = true
-    if (timeout) clearTimeout(timeout)
     if (!symbol.value) return
     const datas = await getData({ symbol: query })
     loading.value = false
@@ -165,13 +127,16 @@ const initData = async () => {
         if (datas && datas.length) {
             let num = 2
             try {
-                num = datas[0].high.toString().split('.')[1].length || 0
+                num = datas[0].price.toString().split('.')[1].length || 0
             } catch {
                 num = 2
             }
             const maxTail = _maxTail(datas[0].high)
             if (num > maxTail) num = maxTail
             chart.setPriceVolumePrecision(num, 2)
+            if (datas[0] && datas[0].timezone) {
+                chart.setTimezone(datas[0].timezone)
+            }
             chart.applyNewData(datas.map(item => {
                 const data = {
                     close: Number(item.close),
@@ -183,13 +148,10 @@ const initData = async () => {
                 }
                 return data
             })) // 重设图表数据
-            if (datas[0] && datas[0].timezone) {
-                chart.setTimezone(datas[0].timezone)
-            }
             subs()
-            timeout = setTimeout(() => {
-                chart.resize()
-            }, 300)
+            resetSize()
+        } else {
+            console.error('没有数据')
         }
     }
 }
@@ -202,24 +164,15 @@ const subs = () => { // 订阅新数据
         socket && socket.on('time', res => {
             if (res.code == 200 && res.symbol == props.symbol) {
                 res.data.forEach(item => {
-                    // chart.updateData({
-                    //     ...item,
-                    //     open: item.price,
-                    //     close: item.price,
-                    //     high: item.price,
-                    //     low: item.price,
-                    //     timestamp: item.timestamp || item.ts
-                    // })
                     chart.updateData({
-                        close: Number(item.close),
-                        high: Number(item.high),
-                        low: Number(item.low),
-                        open: Number(item.open),
-                        timestamp: item.timestamp ? Number(item.timestamp) : Number(item.ts),
-                        volume: Number(item.volume)
+                        ...item,
+                        open: Number(item.price),
+                        close: Number(item.price),
+                        high: Number(item.price),
+                        low: Number(item.price),
+                        timestamp: (item.timestamp || item.ts)
                     })
                 })
-                chart.updateData()
             }
         })
     })
@@ -239,7 +192,7 @@ const getData = (params) => { // 获取数据
         if (rs && rs.length) {
             // 判断数据存储的时间
             const t = Number(sessionStorage.getItem(key + '_time') || 0)
-            if (Date.now() - t > 1 * 60 * 1000) { // 大于5分钟了就去重新请求
+            if (Date.now() - t > 5 * 60 * 1000) { // 大于5分钟了就去重新请求
 
             } else {
                 resolve(rs)
@@ -249,11 +202,11 @@ const getData = (params) => { // 获取数据
         _time(params).then(res => {
             if (res.code == 200) {
                 const dd = res.data.map(item => {
-                    item.open = item.price
-                    item.close = item.price
-                    item.high = item.price
-                    item.low = item.price
-                    item.timestamp = item.timestamp * 1000
+                    item.open = Number(item.price)
+                    item.close = Number(item.price)
+                    item.high = Number(item.price)
+                    item.low = Number(item.price)
+                    item.timestamp = (item.timestamp || item.ts)
                     return item
                 }).reverse()
                 resolve(dd)
@@ -270,15 +223,17 @@ const getData = (params) => { // 获取数据
 }
 
 // 重置图表大小
+let timeout2 = null
 const resetSize = () => {
-    setTimeout(() => {
+    if (timeout2) clearTimeout(timeout2)
+    timeout2 = setTimeout(() => {
         chart.resize()
     }, 500)
 }
 
-
 defineExpose({
-    resetSize
+    resetSize,
+    initData,
 })
 </script>
 
@@ -299,7 +254,7 @@ defineExpose({
         left: 0;
         width: 100%;
         height: 100%;
-        background-color: #fff;
+        // background-color: rgba(0, 0, 0, 0.05);
         z-index: 99;
         // border-right: 1px solid #7F939E;
         // border-bottom: 1px solid #7F939E;
