e1a52634d61ecbe2fd64d1ce222470cfa48e6d14
willam <willam@gmail.com>
2025-01-01 15:35:08 +0800

    fix: 通用余额信息弹窗

--- src/components/AmountDialog.vue
+++ src/components/AmountDialog.vue
@@ -1,43 +1,41 @@
 <template>
 <!-- 余额提示 -->
-<Popup round v-model:show="showAmountDialog" @closed="close()" closeable teleport="body">
-    <div style="width: 6.4rem">
+<BottomPopup  v-model:show="showAmountDialog" :title="$t('trade.ai_opening_pop_title')" @closed="close()" closeable >
 
-      <!-- 标题 -->
-      <div
-        style="text-align: center;font-size: 0.32rem;height:1rem;display: flex;align-items: center;justify-content: center;border:1px solid var(--ex-border-color);">{{$t('trade.ai_opening_pop_title')}}</div>
 
       <!-- 内容 -->
       <div
-        style="display:flex;align-items:center;justify-content:center;text-align:center;background:var(--ex-bg-color2);border:1px solid var(--ex-border-color);border-radius:0.32rem;line-height:0.4rem;margin-top:0.32rem;overflow:hidden;position:relative;margin:0.32rem 0.4rem;">
-        <div
-          style="color:var(--ex-text-color);font-size:0.28rem;font-weight:400;padding:0 0.32rem;height:1.4rem;background-color:var(--ex-bg-color);display:flex;align-items:center;justify-content:center;">
+        class="bg-color3 rounded-[0.4rem] px-[0.12rem] pb-[0.12rem]"
+        style="text-align:center;border-radius:0.32rem;line-height:0.4rem;overflow:hidden;position:relative;margin:0.32rem 0.4rem 0 0.4rem;">
+        <div class="text-color h-[0.84rem] flex items-center justify-center text-[0.32rem]">
           {{ account }}</div>
-        <div style="display:flex;align-items:center;justify-content:center;flex-direction: column;flex:1">
-          <div style="display:flex;align-items:center;justify-content:center;margin-bottom:0.08rem">
-            <div v-if="currency" style="width:0.32rem;height:0.32rem;display:flex;position:relative;top:-0.02rem">
+        <div class="flex items-center justify-center flex-col bg-color p-[0.2rem] rounded-[0.4rem]">
+          <div class="flex items-center justify-center mb-[0.2rem]">
+            <div v-if="currency" class="size-[0.4rem]">
               <img :src="getStaticImgUrl(`/static/img/crypto/${currency.toUpperCase()}.svg`)" />
             </div>
 
-            <span style="font-size:0.28rem;margin-left:0.12rem;color:var(--ex-text-color);font-weight:400">{{ currency }}</span>
+            <span class="text-[0.3rem] ml-[0.12rem] text-color">{{ currency }}</span>
           </div>
-          <b style="font-size:0.4rem;color:var(--ex-primary-color);font-weight:bold">{{ amount }}</b>
+          <div class=" leading-[0.54rem] text-[0.48rem] font-[600] text-primary">{{ amount }}</div>
         </div>
       </div>
 
       <!--  按钮 -->
-      <div
-        style="display: flex;align-items: center;justify-content: space-between;padding: 0 0.4rem;font-size: 0.28rem;margin: 0.64rem 0 0.4rem 0">
-        <div @click="router.push({ name: 'transfer' });close();"
-          style="height: 0.8rem;width:48%;display: flex;align-items: center;justify-content: center;border-radius: 0.64rem;border: 1px solid var(--ex-primary-color);color: var(--ex-primary-color)">
-          {{ $t('market.market_buy_fast_noti_confirm') }}</div>
-        <div @click="router.push({ name: 'topUpCrypto' });close();"
-          style="height: 0.8rem;width:48%;display: flex;align-items: center;justify-content: center;border-radius: 0.64rem;background-color: var(--ex-primary-color);color: var(--ex-text-color--bg-primary);">
-          {{ $t('market.market_buy_fast_noti_cancel') }}</div>
-      </div>
-    </div>
 
-  </Popup>
+      <div
+        class="flex items-center justify-between text-[0.28rem] gap-[0.4rem] flex-wrap"
+        style="padding: 0.6rem 0.4rem 0.76rem 0.4rem;">
+        <div @click="router.push({ name: 'topUpCrypto' });close();"
+          class="h-[1.12rem] flex items-center justify-center font-[600] text-[0.36rem]"
+          style="width:calc(50% - 0.2rem);border-radius: 0.64rem;background-color: var(--ex-white);color: var(--ex-text-color--bg-light);">
+          {{ $t('market.market_buy_fast_noti_cancel') }}</div>
+          <div  class="h-[1.12rem] flex items-center justify-center font-[600] text-[0.36rem]" @click="router.push({ name: 'transfer' });close();"
+          style="width:calc(50% - 0.2rem);border-radius: 0.64rem; color: var(--ex-text-color--bg-primary);background-color: var(--ex-primary-color);">
+          {{ $t('market.market_buy_fast_noti_confirm') }}</div>
+      </div>
+
+  </BottomPopup>
 </template>
 
 <script setup>
@@ -45,6 +43,7 @@ import { Popup } from "vant";
 import { getStaticImgUrl } from "@/utils/index.js"
 import router from "@/router";
 import { watch } from "vue";
+import BottomPopup from "./BottomPopup.vue";
 const props = defineProps({
     show:Boolean,
     currency:String,
