26f1f1d45287c4f5a6fb75a54702254d8325f7ea
eyovvalupe <rylantwcn705172@hotmail.com>
2025-01-06 17:47:52 +0800

    feat:质押挖矿订单历史无数据UI

--- src/views/Staking/Order.vue
+++ src/views/Staking/Order.vue
@@ -1,12 +1,30 @@
 <template>
-    <div class="w-full">
+    <div class="order_page w-full">
         <Top :title="t('Investment Order')" />
+        <Tabs class="mt-[1.5rem] px-[0.2rem]" type="custom-card" v-model:active="active" :swipeable="false" :color="'var(--ex-primary-color)'" shrink @change="onChange">
+            <Tab style="min-width: 2rem" :title="t('In Progress')" name="0">
+                <div class="mt-[0.2rem]">
+                    <NoData />
+                </div>
+            </Tab>
+            <Tab style="min-width: 2rem" :title="t('Completed')" name="1">
+                <div class="mt-[0.2rem]">
+                    <NoData />
+                </div>
+            </Tab>
+        </Tabs>
     </div>
 </template>
 <script setup>
 import Top from '@/components/Top.vue';
 import { useI18n } from 'vue-i18n';
 import { Tabs, Tab } from 'vant';
+import NoData from '@/components/NoData.vue';
 
-const {t} = useI18n();
+const { t } = useI18n();
 </script>
+<style lang="less">
+.order_style {
+
+}
+</style>
