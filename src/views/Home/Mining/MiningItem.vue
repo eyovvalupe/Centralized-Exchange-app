b5b7cd022684e5609aeff82bc2ff61b6a67dddbd
eyovvalupe <rylantwcn705172@hotmail.com>
2025-01-27 12:44:46 +0800

    优化

--- src/views/Home/Mining/MiningItem.vue
+++ src/views/Home/Mining/MiningItem.vue
@@ -20,7 +20,7 @@
                 </div>
             </div>
             <div class="line-box">
-                <SparkLine  :points="getPoints('mining_13213', 80)" :ratio="1" />
+                <SparkLine v-if="loaded" :points="points" :ratio="1" />
             </div>
         </div>
         <div class="btn ripple-btn">{{ t('finance.portfolio_participate') }}</div>
@@ -29,10 +29,17 @@
 
 <script setup>
 import { useI18n } from 'vue-i18n';
-import { getStaticImgUrl, getPoints } from "@/utils/index.js"
+import { getPoints } from "@/utils/index.js"
 import SparkLine from "@/components/SparkLine.vue";
+import { ref, onMounted } from "vue"
 
+const points = ref(getPoints('id-123', 80))
 const { t } = useI18n();
+
+const loaded = ref(false) // 模拟异步
+onMounted(() => {
+    loaded.value = true
+}, 200)
 </script>
 
 <style lang="less">
