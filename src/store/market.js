f35b0b54404f0ef5243eaf94909b8029f10df23a
eyovvalupe <rylantwcn705172@hotmail.com>
2025-02-08 11:35:57 +0800

    FeaT:删除console

--- src/store/market.js
+++ src/store/market.js
@@ -569,7 +569,6 @@ export default {
                 _watchlist()
                     .then(res => {
                         if (res.code == 200 && res.data) {
-                            console.log('watch list  =======>', res.data)
                             commit('setMarketWatchList', res.data)
                             resolve(res.data)
                         } else {
