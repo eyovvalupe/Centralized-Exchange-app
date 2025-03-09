588866e1e4f29794a507570fbcba5c78a793ee7f
eyovvalupe <rylantwcn705172@hotmail.com>
2025-03-09 23:17:07 +0800

    feat:菜单结构

--- src/components/RightMenu.vue
+++ src/components/RightMenu.vue
@@ -5,18 +5,79 @@
     :style="{ visibility: rendered ? 'visible' : '' }"
     @click.self="close"
   >
-    <div class="main"></div>
+    <div class="main">
+      <div class="item" @click="goNext(item.name)" v-for="(item, i) in itemMap">
+        <div class="item_h">
+          <div class="img_container">
+            <img
+              class="img"
+              :class="{ '!opacity-[1]': activeItem != item.name }"
+              v-lazy="getStaticImgUrl(`static/img/menu/${item.name}.svg`)"
+            />
+            <img
+              class="img"
+              :class="{ '!opacity-[1]': activeItem == item.name }"
+              v-lazy="getStaticImgUrl(`static/img/menu/${item.name}_white.svg`)"
+            />
+          </div>
+        </div>
+        <div class="size-[0.28rem]" v-if="!isEmpty(item.sub)">
+          <img
+            v-if="activeItem != item.name"
+            v-lazy="getStaticImgUrl('static/img/menu/down.svg')"
+            alt=""
+          />
+          <img
+            v-if="activeItem == item.name"
+            v-lazy="getStaticImgUrl('static/img/menu/up.svg')"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script setup>
   import { computed, onMounted, watch } from 'vue';
   import store from '@/store';
+  import { getStaticImgUrl } from '@/utils/index.js';
+  import { isEmpty } from '../utils/isEmpty';
+
+  const itemMap = [
+    { name: 'one_click' },
+    { name: 'market',
+    sub: ['optional', 'stock', 'spot', 'futures', 'ai', 'forex', 'commodities']
+     },
+    { name: 'trade',
+    sub: ['stockTrade', 'spotTrade', 'futuresTrade', 'aiTrade', 'forexTrade', 'commoditiesTrade']
+     },
+    { name: 'finance',
+    sub: ['copy', 'borrow', 'stake', 'ipo']
+     },
+    { name: 'assets',
+      sub: ['assets', 'deposit', 'withdraw', 'transfer', 'record']
+     },
+    { name: 'order' },
+    { name: 'user',
+      sub: ['user', 'account', 'kyc', 'google', 'safety']
+     },
+    { name: 'referral' },
+    { name: 'lang' },
+    { name: 'support' },
+  ];
 
   const rendered = ref(false);
   const status = computed(() => store.state.showRightMenu);
 
+  const activeItem = ref('');
+
+  const goNext = (val) => {
+    activeItem.value = val;
+  };
+
   const close = () => {
     store.commit('setShowRightMenu', false);
+    activeItem.value = '';
   };
 
   onMounted(() => {
@@ -44,7 +105,43 @@
       background-color: var(--ex-bg-color9);
       width: 6.8rem;
       height: calc(100vh - 50px);
+      padding: 0.26rem 0;
+      display: flex;
+      flex-direction: column;
 
+      .item {
+        width: 100%;
+        height: 0.92rem;
+        display: flex;
+        padding: 0 0.4rem 0 0.32rem;
+        justify-content: space-between;
+        align-items: center;
+        color: var(--ex-white);
+        &:hover {
+          background-color: var(--ex-bg-white2);
+          color: var(--ex-primary-color);
+        }
+
+        .item_h {
+          height: 0.92rem;
+          display: flex;
+          gap: 0.16rem;
+          align-items: center;
+
+          .img_container {
+            width: 0.48rem;
+            height: 0.48rem;
+            position: relative;
+
+            .img {
+              position: absolute;
+              top: 0;
+              left: 0;
+              opacity: 0;
+            }
+          }
+        }
+      }
     }
   }
   .fade_in_action {
